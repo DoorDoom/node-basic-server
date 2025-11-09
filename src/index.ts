6897267e4d4f763b2885495f29a1ddfe2cf07899
Alina Vagner <ali.vag977@gmail.com>
2025-11-09 17:52:38 +0300

    create basic server

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import http from 'http';
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello, World!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
